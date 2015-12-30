3cd7f69c05e777a2d039058ff1e4714ccfe44350
hhg128@gmail.com <hg811421!>
2015-12-30 20:01:35 +0900

    Add Object class

---
+++ SimpleGL/Object.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "Object.h"
+
+namespace SimpleGL
+{
+	Object::Object()
+	{
+	}
+
+
+	Object::~Object()
+	{
+	}
+}
---
+++ SimpleGL/Object.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace SimpleGL
+{
+	class Object
+	{
+	public:
+		Object();
+		virtual ~Object();
+	};
+}
